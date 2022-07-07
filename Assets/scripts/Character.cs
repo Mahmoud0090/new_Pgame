b5238bb4a386cc479d7197c2e0514689dc1240fb
Mahmoud0090 <mmh.00090@gmail.com>
2022-07-07 08:10:24 +0300

    more control

--- Assets/scripts/Character.cs
+++ Assets/scripts/Character.cs
@@ -9,6 +9,12 @@ public class Character : MonoBehaviour
     Rigidbody2D rd;
     [SerializeField] float jumpForce = 100f;
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float startTimeBetweenJumping = 2f;
+    [SerializeField] bool reverseMove = false;
+    [SerializeField] bool jumpingControl = true;
+    [SerializeField] float selfJumpingAddForce = 2f;
+
+    private float timeBetweenJumping;
 
     private int jumpCounter = 0;
 
@@ -17,12 +23,20 @@ public class Character : MonoBehaviour
     {
         rd = GetComponent<Rigidbody2D>();
         col = GetComponent<BoxCollider2D>();
+        timeBetweenJumping = startTimeBetweenJumping;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Jump();
+        if (jumpingControl)
+        {
+            NormalJumping();
+        }
+        else
+        {
+            OutOfControlJumping();
+        }
         Move();
     }
 
@@ -30,10 +44,19 @@ public class Character : MonoBehaviour
     {
         float HorizontalMove = Input.GetAxisRaw("Horizontal");
 
-        transform.Translate(new Vector2(1, 0) * HorizontalMove * moveSpeed * Time.deltaTime);
+        if (!reverseMove)
+        {
+            transform.Translate(new Vector2(1, 0) * HorizontalMove * moveSpeed * Time.deltaTime);
+        }
+
+        else
+        {
+            transform.Translate(new Vector2(1, 0) * -HorizontalMove * moveSpeed * Time.deltaTime);
+        }
+        
     }
 
-    private void Jump()
+    private void NormalJumping()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -47,6 +70,19 @@ public class Character : MonoBehaviour
                 rd.velocity = Vector2.up * jumpForce;
                 jumpCounter++;
             }
+        }
+    }
+
+    private void OutOfControlJumping()
+    {
+        if (timeBetweenJumping <= 0 && col.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            rd.velocity = Vector2.up * jumpForce * selfJumpingAddForce;
+            timeBetweenJumping = startTimeBetweenJumping;
+        }
+        else
+        {
+            timeBetweenJumping -= Time.deltaTime;
         }
     }
 }
