bfd6f1922fba3a0a9c55d9428979b4f87ec09f55
Mahmoud0090 <mmh.00090@gmail.com>
2022-07-12 23:15:34 +0300

    follower camera bug fixed

--- Assets/scripts/FollowerCamera.cs
+++ Assets/scripts/FollowerCamera.cs
@@ -6,14 +6,29 @@ public class FollowerCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] GameObject player;
+    [SerializeField] GameObject leftLimit;
+    [SerializeField] GameObject rightLimit;
+    [SerializeField] float limitOffset = 28;
     void Start()
     {
-        transform.position = new Vector3(player.transform.position.x , transform.position.y , -10);
+        transform.position = new Vector3(leftLimit.transform.position.x + limitOffset, transform.position.y , -10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, -10);
+        if(player.transform.position.x <= leftLimit.transform.position.x + limitOffset)
+        {
+            transform.position = new Vector3(leftLimit.transform.position.x + limitOffset, transform.position.y, -10);
+        }
+        else if(player.transform.position.x >= rightLimit.transform.position.x - limitOffset)
+        {
+            transform.position = new Vector3(rightLimit.transform.position.x - limitOffset, transform.position.y, -10);
+        }
+        else
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, -10);
+        }
+        
     }
 }
